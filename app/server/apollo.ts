af56f8e59f828544b578497500496ceccd99ba18
Talasan Nicholson <talasan.nicholson@gmail.com>
2018-11-19 18:01:24 -0600

    limit to 3k

--- app/server/apollo.ts
+++ app/server/apollo.ts
@@ -3,15 +3,7 @@ import * as express from 'express'
 import schema from './schema'
 
 const dev = process.env.NODE_ENV !== 'production'
-const port = parseInt(process.env.PORT, 10) || 3000
 const app = express()
 
-const server = app.use(schema({ app, dev })).listen(port, err => {
-  if (err) {
-    throw err
-  }
-
-  console.log(`>ready on http://[::1]:${port}\n🚀`)
-})
-
+const server = app.use(schema({ app, dev })).listen(3000)
 process.on('exit', () => server.close())
